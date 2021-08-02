c5e9d05f93b2ef0c970af0ca0bc55c0d8b93e16c
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-08-02 17:48:27 +0900

    use render_template

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello, world'
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
