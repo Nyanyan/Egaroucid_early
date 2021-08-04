8f62061300e306dfe8946efce56e826de0a51804
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-08-05 05:10:29 +0900

    making move funcs

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,22 @@
 function disableScroll(event) {
     event.preventDefault();
 }
-document.addEventListener('touchmove', disableScroll, { passive: false });
+document.addEventListener('touchmove', disableScroll, {passive: false});
+function move(r, c) {
+    var post = $.ajax({
+        type: "POST",
+        url: "/move",
+        data: {r: r, c: c},
+        async: false,
+        dataType: "json",
+        success: function(response) {
+            console.log(response);
+            count = response.count;
+        }, 
+        error: function(error) {
+            console.log("Error occurred in move().");
+            console.log(error);
+            alert("An error occurred. Please try again.");
+        }
+    })
+}
