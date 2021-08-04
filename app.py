a59cc208da09bdade818d85bb02ee2d96a27b24f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-08-04 23:16:43 +0900

    trying draw stones

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from flask import *
 
+hw = 8
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    grid = [[-1 for _ in range(hw)] for _ in range(hw)]
+    grid[3][3] = 1
+    grid[3][4] = 0
+    grid[4][3] = 0
+    grid[4][4] = 1
+    return render_template('base.html', grid=grid)
 
 if __name__ == '__main__':
     app.run()
