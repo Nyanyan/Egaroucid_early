fc351fef93f28e9b830df79def49212c88dda6d8
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-08-08 00:32:44 +0900

    trying to deploy

--- ai.cpp
+++ ai.cpp
@@ -569,43 +569,36 @@ void init(){
     };
     static double weight_buf[10];
     int all_idx = 0;
-    cerr << "a" << endl;
     for (i = 0; i < hw2; ++i)
         eval_param.avg_canput[i] = params[all_idx++];
-    cerr << "b" << endl;
     for (i = 0; i < 10; i++)
         weight_buf[i] = params[all_idx++];
-    cerr << "c" << endl;
     for (i = 0; i < hw2; i++)
         eval_param.weight[i] = weight_buf[translate[i]];
-    cerr << "d" << endl;
     for (i = 0; i < param_num; ++i)
         eval_param.weight_sme[i] = params[all_idx++];
-    cerr << "e" << endl;
     all_idx = 0;
     for (i = 0; i < board_index_num; ++i)
         board_param.pattern_space[i] = consts[all_idx++];
-    cerr << "f" << endl;
     for (i = 0; i < board_index_num; ++i){
         for (j = 0; j < board_param.pattern_space[i]; ++j)
             board_param.board_translate[i][j] = consts[all_idx++];
     }
-    cerr << "g" << endl;
     for (i = 0; i < pattern_num; ++i)
         eval_param.pattern_space[i] = consts[all_idx++];
-    cerr << "h" << endl;
     for (i = 0; i < pattern_num; ++i)
         eval_param.pattern_variation[i] = consts[all_idx++];
-    cerr << "i" << endl;
+    cerr << "a" << endl;
     for (i = 0; i < pattern_num; ++i){
         for (j = 0; j < eval_param.pattern_variation[i]; ++j){
             for (k = 0; k < eval_param.pattern_space[i]; ++k){
-                eval_param.pattern_translate[i][j][k][0] =consts[all_idx] / hw;
-                eval_param.pattern_translate[i][j][k][1] = consts[all_idx++] % hw;
+                eval_param.pattern_translate[i][j][k][0] = consts[all_idx] / hw;
+                eval_param.pattern_translate[i][j][k][1] = consts[all_idx] % hw;
+                ++all_idx;
             }
         }
     }
-    cerr << "j" << endl;
+    cerr << "b" << endl;
     int idx;
     for (i = 0; i < hw2; ++i){
         idx = 0;
