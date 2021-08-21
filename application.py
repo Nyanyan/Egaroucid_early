fa23767391e1ffd9f11bc2bcc904a96c8aea145f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2021-08-21 14:58:40 +0900

    parallel and display

--- application.py
+++ application.py
@@ -7,11 +7,12 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 hw = 8
-reload_limit = 1.0
+num_ais = 2
 
 application = Flask(__name__)
 limiter = Limiter(application, key_func=get_remote_address, default_limits=["500 per day", "50 per hour"])
-ai = subprocess.Popen('./ai.out'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+ais = [subprocess.Popen('./ai.out'.split(), stdin=subprocess.PIPE, stdout=subprocess.PIPE) for _ in range(num_ais)]
+vacant = [True for _ in range(num_ais)]
 
 @application.route('/')
 @limiter.limit("1/second", override_defaults=False)
@@ -50,9 +51,20 @@ def call_ai():
     for y in range(hw):
         for x in range(hw):
             stdin += '0' if grid[y][x] == 0 else '1' if grid[y][x] == 1 else '.'
-    ai.stdin.write(stdin.encode('utf-8'))
-    ai.stdin.flush()
-    r, c, s = [float(i) for i in ai.stdout.readline().decode().strip().split()]
+    ai_num = -1
+    while True:
+        for i in range(num_ais):
+            if vacant[i]:
+                ai_num = i
+                vacant[i] = False
+                break
+        else:
+            continue
+        break
+    ais[ai_num].stdin.write(stdin.encode('utf-8'))
+    ais[ai_num].stdin.flush()
+    r, c, s = [float(i) for i in ais[ai_num].stdout.readline().decode().strip().split()]
+    vacant[ai_num] = True
     r = int(r)
     c = int(c)
     print(r, c, s)
@@ -61,4 +73,4 @@ def call_ai():
 
 if __name__ == '__main__':
     print('start python')
-    application.run()
+    application.run(threaded=True)
